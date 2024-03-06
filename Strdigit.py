ca38814a588db15bcd2a9333c77bf3c3017c146c
mandee33 <mandee33@shernet.sheridancollege.ca>
2024-03-06 17:39:20 -0500

    projects

---
+++ Strdigit.py
@@ -0,0 +1,8 @@
+x=(input('string:'))
+def z(str):
+    if str.isdigit():
+        return True
+    else:
+        return False
+i=z(x)
+print(i)
---
+++ replace.py
@@ -0,0 +1,8 @@
+# list=[2,5]
+str='ABCDEFGH'
+char='X'
+# def s_replace(list,str,char):
+#     str.replace('C', [2])
+print(str.replace('C',char))
+#     str.replace('F', [5])
+print(str.replace('F', char))
